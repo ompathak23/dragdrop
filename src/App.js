5392328942e2fe0c5f513c64cd120aeac271ace3
Om Pathak <ompatthak@gmail.com>
2022-12-05 09:36:50 +0530

    Drag and Drop functionality added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
+import React, { useState, useRef } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const draggingItem = useRef();
+  const dragOverItem = useRef();
+
+  const [list, setList] = useState([
+    'Shopping in Barcelona',
+    'Famous Shopping Streets',
+    'Shopping Malls',
+    'Markets',
+    'Opening times',
+    'Sales Tax Refunds',
+    'You may also be interested in',
+    'Where to Stay'
+  ]);
+
+  const handleDragStart = (e, position) => {
+    draggingItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+
+  const handleDragEnter = (e, position) => {
+    dragOverItem.current = position;
+    console.log(e.target.innerHTML);
+    const listCopy = [...list];
+    console.log(draggingItem.current, dragOverItem.current);
+    const draggingItemContent = listCopy[draggingItem.current];
+    listCopy.splice(draggingItem.current, 1);
+    listCopy.splice(dragOverItem.current, 0, draggingItemContent);
+
+    draggingItem.current = dragOverItem.current;
+    dragOverItem.current = null;
+    setList(listCopy);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 id="title">Outlines</h1>
+      {list &&
+        list.map((item, index) => (
+          <h1
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={(e) => handleDragEnter(e, index)}
+            key={index}
+            draggable
+          >
+            {item}
+          </h1>
+        ))}
+    </>
   );
-}
+};
 
 export default App;
