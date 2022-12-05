899a0a828215ecaf69eb1fb5387b42fd591a449d
Om Pathak <ompatthak@gmail.com>
2022-12-05 09:50:04 +0530

    Added Comments

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState, useRef } from 'react';
 import './App.css';
 
 const App = () => {
-  const draggingItem = useRef();
-  const dragOverItem = useRef();
+  const draggingItem = useRef(); // To keep track of the item by its position
+  const dragOverItem = useRef(); // To keep track of the item over which the other item is hovering
+
+  // List of outlines
 
   const [list, setList] = useState([
     'Shopping in Barcelona',
@@ -16,16 +18,20 @@ const App = () => {
     'Where to Stay'
   ]);
 
+  // This function keeps tracks of the item being dragged;
   const handleDragStart = (e, position) => {
     draggingItem.current = position;
-    console.log(e.target.innerHTML);
+    //console.log(e.target.innerHTML);
   };
 
+  // This function keeps tracks of the item over which the dragged item is hovering
+  // This also reshuffles the list whenever we drag an item over another item
   const handleDragEnter = (e, position) => {
     dragOverItem.current = position;
-    console.log(e.target.innerHTML);
+    //console.log(e.target.innerHTML);
+
     const listCopy = [...list];
-    console.log(draggingItem.current, dragOverItem.current);
+    //console.log(draggingItem.current, dragOverItem.current);
     const draggingItemContent = listCopy[draggingItem.current];
     listCopy.splice(draggingItem.current, 1);
     listCopy.splice(dragOverItem.current, 0, draggingItemContent);
@@ -42,10 +48,10 @@ const App = () => {
         list.map((item, index) => (
           <h1
             onDragStart={(e) => handleDragStart(e, index)}
-            onDragOver={(e) => e.preventDefault()}
+            onDragOver={(e) => e.preventDefault()} // To prevent shadow from going back to its original position
             onDragEnter={(e) => handleDragEnter(e, index)}
             key={index}
-            draggable
+            draggable // draggable prop makes the children of the list draggable
           >
             {item}
           </h1>
